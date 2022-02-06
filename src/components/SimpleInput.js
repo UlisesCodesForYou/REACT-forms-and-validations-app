c58e0303a54249617193541a42c04b1e6537e955
UlisesCodesForYou <76757797+UlisesCodesForYou@users.noreply.github.com>
2022-02-05 20:19:52 -0800

    Slimmed down the SimpleInput js.

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -2,31 +2,52 @@ import { useState } from "react";
 
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState("");
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  const enteredNameIsValid = enteredName.trim() !== "";
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
   };
 
+  const nameInputBlur = (event) => {
+    setEnteredNameTouched(true);
+  };
+
   const formSubmissionHandler = (event) => {
     event.preventDefault();
-
-    if (enteredName.trim() === "") {
+    setEnteredNameTouched(true); // This state pretty much states that the user touched the form one way or another
+    if (!enteredNameIsValid) {
+      // This prevents the user from sending an empty string to the database. I also used the .trim method to remove any excess white space
       return;
     } else {
+      setEnteredNameTouched(true);
+
       console.log(enteredName);
       setEnteredName(""); //This will reset the form when it is submitted.  I have set it to an empty string.
+      setEnteredNameTouched(false);
     }
   };
+
+  const inputFieldClasses = nameInputIsInvalid
+    ? "form-control invalid"
+    : "form-control";
+
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className="form-control">
+      <div className={inputFieldClasses}>
         <label htmlFor="name">Your Name</label>
         <input
           type="text"
           id="name"
+          onBlur={nameInputBlur}
           onChange={nameInputChangeHandler}
           value={enteredName}
         />
+        {nameInputIsInvalid && (
+          <p className="error-text">Please type in your name.</p>
+        )}
       </div>
       <div className="form-actions">
         <button>Submit</button>
